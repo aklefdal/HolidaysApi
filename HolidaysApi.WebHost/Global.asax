2304afec01eac3db33737b8d39e37b6b12e46072
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2014-02-02 22:13:00 +0100

    Converted F#-library to a wewbhost

---
+++ HolidaysApi.WebHost/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Inherits="Aklefdal.Holidays.HttpApi.HttpHost.Global" %>
---
+++ HolidaysApi.WebHost/Infrastructure.fs
@@ -0,0 +1,15 @@
+﻿namespace Aklefdal.Holidays.HttpApi.HttpHost
+
+open System
+open System.Web.Http
+
+type HttpRouteDefaults = { Controller : string; Id : obj }
+
+type Global() =
+    inherit System.Web.HttpApplication()
+    member this.Application_Start (sender : obj) (e : EventArgs) =
+        GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+            "DefaultAPI",
+            "{controller}/{id}",
+            { Controller = "Home"; Id = RouteParameter.Optional }) |> ignore
+
