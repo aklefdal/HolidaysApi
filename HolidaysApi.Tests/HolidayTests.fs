a9a46fa779856b3f6fd172d63a3cb45b318187ba
bjartwolf <bjartnes@gmail.com>
2014-12-13 14:54:04 +0100

    Added Holiday tests

---
+++ HolidaysApi.Tests/HolidayTests.fs
@@ -0,0 +1,15 @@
+﻿module HolidayTests
+
+open NUnit.Framework
+open FsCheck
+open System 
+open Aklefdal.Holidays.HttpApi.Holidays
+
+let ``Langfredag is fredag in Norway`` (date: DateTime) =
+    let norwegianHolidays =  HolidaysNO date.Year 
+    let (_, langfredag)= Seq.find (fun (name, holidayDate) -> name = "Langfredag") norwegianHolidays
+    langfredag.DayOfWeek = DayOfWeek.Friday
+
+[<Test>]
+let ``Test that langfredag is fredag``() = 
+    Check.QuickThrowOnFailure ``Langfredag is fredag in Norway``
