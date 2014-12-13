735caad2041e9f6a153f99d62109e3ca3b2f9ca7
bjartwolf <bjartnes@gmail.com>
2014-12-13 17:12:15 +0100

    Used some fancy FSCheck features (preoperty)

--- HolidaysApi.Tests/EasterTests.fs
+++ HolidaysApi.Tests/EasterTests.fs
@@ -2,41 +2,29 @@
 
 open NUnit.Framework
 open FsCheck
+open FsCheck.NUnit
 open System 
 open Aklefdal.Holidays.HttpApi.Computus
 
+[<Property>]
 let ``Easter Day is Sunday`` (date: DateTime) =
     let easter = EasterDay2 date.Year
     easter.DayOfWeek = DayOfWeek.Sunday
 
+[<Property>]
 let ``Algorithms are equal`` (date: DateTime) =
     EasterDay date.Year = EasterDay2 date.Year
 
-[<Test>]
-let ``Test Algorithms are equal`` () = 
-    Check.QuickThrowOnFailure ``Algorithms are equal``
-
-[<Test>]
-let ``Test that easterday is sunday``() = 
-    Check.QuickThrowOnFailure ``Easter Day is Sunday``
-
+[<Property>]
 let ``In Western Christianity, using the Gregorian calendar, Easter always falls on a Sunday between 22 March and 25 April inclusive``(date: DateTime) =
     let easter = EasterDay date.Year
     let firstPossibleDay = new DateTime(date.Year, 3, 22)
     let lastPossibleDay = new DateTime(date.Year, 4, 25)
     easter >= firstPossibleDay && easter <= lastPossibleDay && easter.DayOfWeek = DayOfWeek.Sunday
 
+[<Property>]
 let ``Alg2: In Western Christianity, using the Gregorian calendar, Easter always falls on a Sunday between 22 March and 25 April inclusive``(date: DateTime) =
     let easter = EasterDay2 date.Year
     let firstPossibleDay = new DateTime(date.Year, 3, 22)
     let lastPossibleDay = new DateTime(date.Year, 4, 25)
     easter >= firstPossibleDay && easter <= lastPossibleDay 
-
-
-[<Test>]
-let ``Test that In Western Christianity, using the Gregorian calendar, Easter always falls on a Sunday between 22 March and 25 April inclusive``() =
-    Check.QuickThrowOnFailure ``In Western Christianity, using the Gregorian calendar, Easter always falls on a Sunday between 22 March and 25 April inclusive``
-
-[<Test>]
-let ``Alg2: Test that In Western Christianity, using the Gregorian calendar, Easter always falls on a Sunday between 22 March and 25 April inclusive``() =
-    Check.QuickThrowOnFailure ``Alg2: In Western Christianity, using the Gregorian calendar, Easter always falls on a Sunday between 22 March and 25 April inclusive``
--- HolidaysApi.Tests/HolidayTests.fs
+++ HolidaysApi.Tests/HolidayTests.fs
@@ -3,25 +3,25 @@
 open NUnit.Framework
 open FsCheck
 open System 
+open FsCheck.NUnit
 open Aklefdal.Holidays.HttpApi.Holidays
 
+[<Property>]
 let ``Langfredag is fredag in Norway`` (date: DateTime) =
     let norwegianHolidays =  HolidaysNO date.Year 
     let (_, langfredag)= Seq.find (fun (name, _) -> name = "Langfredag") norwegianHolidays
     langfredag.DayOfWeek = DayOfWeek.Friday
 
-[<Test>]
-let ``Test that langfredag is fredag``() = 
-    Check.QuickThrowOnFailure ``Langfredag is fredag in Norway``
-
 //http://no.wikipedia.org/wiki/Kristi_himmelfartsdag
 //Kristi himmelfartsdag vil alltid falle på en torsdag 39 dager (5½ uke) etter 1. påskedag, dvs. i tidsrommet 30. april – 3. juni.
 
+[<Property>]
 let ``Kristi himmelfartsdag is thursday`` (date: DateTime) =
     let norwegianHolidays =  HolidaysNO date.Year 
     let (_, himmelfartsdag)= Seq.find (fun (name, _) -> name = "Kristi Himmelfartsdag") norwegianHolidays
     himmelfartsdag.DayOfWeek = DayOfWeek.Thursday
 
+[<Property>]
 let ``Kristi himmelfartsdag is between 30. april – 3. juni`` (date: DateTime) =
     let norwegianHolidays =  HolidaysNO date.Year 
     let (_, himmelfartsdag)= Seq.find (fun (name, _) -> name = "Kristi Himmelfartsdag") norwegianHolidays
@@ -29,11 +29,3 @@ let ``Kristi himmelfartsdag is between 30. april – 3. juni`` (date: DateTime) 
     let firstPossibleDay = new DateTime(year, 4, 30)
     let lastPossibleDay = new DateTime(year, 6, 3)
     himmelfartsdag >= firstPossibleDay && himmelfartsdag <= lastPossibleDay 
-
-[<Test>]
-let ``Test that Kristi himmelfartsdag is thursday``() = 
-    Check.QuickThrowOnFailure ``Kristi himmelfartsdag is thursday``
-
-[<Test>]
-let ``Test that Kristi himmelfartsdag is in allowed range``() = 
-    Check.QuickThrowOnFailure ``Kristi himmelfartsdag is between 30. april – 3. juni`` 
