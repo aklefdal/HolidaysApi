a33f0c28d3686b013ba6e78978ca2079ca596cbf
bjartwolf <bjartnes@gmail.com>
2014-12-13 17:03:18 +0100

    Added check that algorithms are equal

--- HolidaysApi.Tests/EasterTests.fs
+++ HolidaysApi.Tests/EasterTests.fs
@@ -9,6 +9,13 @@ let ``Easter Day is Sunday`` (date: DateTime) =
     let easter = EasterDay2 date.Year
     easter.DayOfWeek = DayOfWeek.Sunday
 
+let ``Algorithms are equal`` (date: DateTime) =
+    EasterDay date.Year = EasterDay2 date.Year
+
+[<Test>]
+let ``Test Algorithms are equal`` () = 
+    Check.QuickThrowOnFailure ``Algorithms are equal``
+
 [<Test>]
 let ``Test that easterday is sunday``() = 
     Check.QuickThrowOnFailure ``Easter Day is Sunday``
@@ -17,13 +24,14 @@ let ``In Western Christianity, using the Gregorian calendar, Easter always falls
     let easter = EasterDay date.Year
     let firstPossibleDay = new DateTime(date.Year, 3, 22)
     let lastPossibleDay = new DateTime(date.Year, 4, 25)
-    easter >= firstPossibleDay && easter <= lastPossibleDay 
+    easter >= firstPossibleDay && easter <= lastPossibleDay && easter.DayOfWeek = DayOfWeek.Sunday
 
 let ``Alg2: In Western Christianity, using the Gregorian calendar, Easter always falls on a Sunday between 22 March and 25 April inclusive``(date: DateTime) =
     let easter = EasterDay2 date.Year
     let firstPossibleDay = new DateTime(date.Year, 3, 22)
     let lastPossibleDay = new DateTime(date.Year, 4, 25)
     easter >= firstPossibleDay && easter <= lastPossibleDay 
+
 
 [<Test>]
 let ``Test that In Western Christianity, using the Gregorian calendar, Easter always falls on a Sunday between 22 March and 25 April inclusive``() =
