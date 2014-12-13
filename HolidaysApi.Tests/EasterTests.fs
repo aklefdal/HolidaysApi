1189cd4f1f57a18cc6567b18a707ba3ef3c95455
bjartwolf <bjartnes@gmail.com>
2014-12-13 14:36:38 +0100

    More cleanup and simplifications using built in date provider
    Only actually use dates between 1900 and 2100 i think

--- HolidaysApi.Tests/EasterTests.fs
+++ HolidaysApi.Tests/EasterTests.fs
@@ -2,30 +2,28 @@
 
 open NUnit.Framework
 open FsCheck
+open System 
 open Aklefdal.Holidays.HttpApi.Computus
 
 //http://en.wikipedia.org/wiki/Easter
-let ``Easter is betweeen March and April`` year =
-    let easter = EasterDay2 year 
+let ``Easter is in March and April`` (date: DateTime) =
+    let easter = EasterDay2 date.Year
     let easterMonth = easter.Month
     match easterMonth with
         | 3 -> true
         | 4 -> true
         | _ -> false
 
-let ``Easter Day is Sunday`` year =
-    let easter = EasterDay2 year 
+let ``Easter Day is Sunday`` (date: DateTime) =
+    let easter = EasterDay2 date.Year
     match easter.DayOfWeek with
         | System.DayOfWeek.Sunday -> true
         | _ -> false
 
-let smallYears = 
-    FsCheck.Gen.choose(0,9999) |> Arb.fromGen
-
 [<Test>]
 let ``Test that easterday is sunday``() = 
-    Check.QuickThrowOnFailure (Prop.forAll (smallYears) ``Easter Day is Sunday``)
+    Check.QuickThrowOnFailure ``Easter Day is Sunday``
 
 [<Test>]
 let ``Test that easter is in march or april``() = 
-    Check.QuickThrowOnFailure (Prop.forAll (smallYears) ``Easter is betweeen March and April``)
+    Check.QuickThrowOnFailure ``Easter is in March and April``
