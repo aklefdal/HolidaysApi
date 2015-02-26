f1112acab07e804698950a0bca92f05b0bc751cd
bjartwolf <bjartnes@gmail.com>
2015-02-26 22:08:07 +0100

    remove version in fake

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 cls
 if not exist "tools\FAKE\tools\Fake.exe" (
-    ".nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion" "-Version 3.17.13"
+    ".nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
 )
 "tools\FAKE\tools\Fake.exe" "build.fsx"
