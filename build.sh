7e39e2bd5c11a29b218d934dfd9604cbc0d13b5a
bjartwolf <bjartnes@gmail.com>
2015-02-26 06:19:16 +0100

    installing fake

--- build.sh
+++ build.sh
@@ -7,16 +7,16 @@ then
   if [ $exit_code -ne 0 ]; then
     exit $exit_code
   fi
-
-  packages/FAKE.3.17.9/tools/FAKE.exe build.fsx $@
+  .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+  mono packages/FAKE/tools/FAKE.exe build.fsx $@
 else
   # use mono
 
-  mono packages/FAKE.3.17.9/tools/ restore
+  mono packages/FAKE/tools/ restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
     exit $exit_code
   fi
   
-  mono packages/FAKE.3.17.9/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 fi
