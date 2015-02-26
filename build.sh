da55ecb4d60f8ffc3f038c8d6dbacd5e167a7104
bjartwolf <bjartnes@gmail.com>
2015-02-26 06:34:01 +0100

    little random guessint at this point
    tooooooooooo tired

--- build.sh
+++ build.sh
@@ -7,16 +7,16 @@ then
   if [ $exit_code -ne 0 ]; then
     exit $exit_code
   fi
-  .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
-  mono packages/FAKE/tools/FAKE.exe build.fsx $@
+  .nuget/NuGet.exe install FAKE -OutputDirectory tools -ExcludeVersion -Prerelease
+  mono tools/FAKE/tools/FAKE.exe build.fsx $@
 else
   # use mono
 
-  mono packages/FAKE/tools/ restore
+  mono tools/FAKE/tools/ restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
     exit $exit_code
   fi
   
-  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+  mono tools/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 fi
