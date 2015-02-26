c0eb04fb62862a45c13264201f04c78f10076029
bjartwolf <bjartnes@gmail.com>
2015-02-26 06:11:23 +0100

    screw it use it from packages

--- build.sh
+++ build.sh
@@ -8,15 +8,15 @@ then
     exit $exit_code
   fi
 
-  tools/FAKE/tools/FAKE.exe build.fsx $@
+  packages/FAKE.3.17.9/tools/FAKE.exe build.fsx $@
 else
   # use mono
 
-  mono tools/FAKE/tools/FAKE.exe restore
+  mono packages/FAKE.3.17.9/tools/ restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
     exit $exit_code
   fi
   
-  mono tools/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+  mono packages/FAKE.3.17.9/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 fi
