ca231d594e7e783dacf6319d1bf6582ae4084664
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2015-07-23 18:57:06 +0200

    Implementerte CachecontrolHeader

--- HolidaysApi.WebHost/Infrastructure.fs
+++ HolidaysApi.WebHost/Infrastructure.fs
@@ -9,23 +9,27 @@ open System.Threading.Tasks;
 
 type HttpRouteDefaults = { Controller : string; Id : obj }
 
-//type SetResponseCacheHeadersHandler() =
-//    inherit DelegatingHandler()
-//
-//    let setHeader task:Task<HttpResponseMessage> = 
-//        let mutable response = task.Result
-//        response.Headers.CacheControl.Public <- true
-//        response.Headers.CacheControl.MaxAge <- new Nullable<TimeSpan>(TimeSpan.FromDays(30.0))
-//        response
-//
-//    override this.SendAsync(request:HttpRequestMessage, cancellationToken:CancellationToken) =
-//        let response = base.SendAsync(request, cancellationToken).ContinueWith(fun task -> setHeader task)
-//        response
+type SetResponseCacheHeadersHandler() =
+    inherit DelegatingHandler()
+
+    let getCacheControlHeader =
+        let cacheControl = new Headers.CacheControlHeaderValue()
+        cacheControl.Public <- true
+        cacheControl.MaxAge <- new Nullable<TimeSpan>(TimeSpan.FromDays(30.0))
+        cacheControl
+
+    override this.SendAsync(request:HttpRequestMessage, cancellationToken:CancellationToken) = 
+        base.SendAsync(request, cancellationToken)
+            .ContinueWith(fun (t : Task<HttpResponseMessage>) -> 
+                          let resp = t.Result
+                          resp.Headers.CacheControl <- getCacheControlHeader
+                          resp
+                         )
 
 type Global() =
     inherit System.Web.HttpApplication()
     member this.Application_Start (sender : obj) (e : EventArgs) =
         GlobalConfiguration.Configuration.Formatters.XmlFormatter.UseXmlSerializer <- true
         Configure GlobalConfiguration.Configuration
-//        GlobalConfiguration.Configuration.MessageHandlers.Add(new SetResponseCacheHeadersHandler());
+        GlobalConfiguration.Configuration.MessageHandlers.Add(new SetResponseCacheHeadersHandler());
         
